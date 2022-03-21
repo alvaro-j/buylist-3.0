aff3a1bfade54c02b8b43e128d56008fbf3133f4
alvaro-j <alvarojaime360@gmail.com>
2022-03-21 09:49:04 -0300

    add comment and some states

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-const App: React.FC = () => {
+const App: React.FC = () => { // FC stands for functional component
+	const [recipes, setRecipes] = React.useState<[]>([]);
+	const [search, setSearch] = React.useState<string>("");
 	return <div>App</div>;
 };
 
