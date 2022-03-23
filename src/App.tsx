83747990e216dfc4ac3fb989fd3a400bf5ae64d6
alvaro-j <alvarojaime360@gmail.com>
2022-03-23 08:32:19 -0300

    add initial code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 
-const App: React.FC = () => { // FC stands for functional component
-	const [recipes, setRecipes] = React.useState<[]>([]);
-	const [search, setSearch] = React.useState<string>("");
-	return <div>App</div>;
+const App: React.FC = () => {
+	// FC stands for functional component
+	return (
+		<div>
+			<header>
+				<form >
+					<input
+						type="text"
+						name="items"
+						id="items"
+						required
+						placeholder="Which items would you like to buy?"
+						value=""
+					/>
+					<button type="submit">Add</button>
+				</form>
+			</header>
+		</div>
+	);
 };
 
 export default App;
