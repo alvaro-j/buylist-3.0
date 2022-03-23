4b64c2acee9de1f5f0ccefb58857a8265b3851e6
alvaro-j <alvarojaime360@gmail.com>
2022-03-23 08:50:45 -0300

    import FormInput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import React from "react";
+import FormInput from "./components/FormInput";
 
 const App: React.FC = () => {
 	// FC stands for functional component
 	return (
-		<div>
-			<header>
-				<form >
-					<input
-						type="text"
-						name="items"
-						id="items"
-						required
-						placeholder="Which items would you like to buy?"
-						value=""
-					/>
-					<button type="submit">Add</button>
-				</form>
-			</header>
+		<div className="main">
+			<h1>
+				buy<span>List</span>
+			</h1>
+			<FormInput />
 		</div>
 	);
 };
