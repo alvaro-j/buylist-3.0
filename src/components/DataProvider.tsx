45b7c2045b902a0631177edde3636f696e4930e0
alvaro-j <alvarojaime360@gmail.com>
2022-03-24 08:07:00 -0300

    create initial context

---
+++ src/components/DataProvider.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const DataProvider: React.FC = () => {
+  const [items, setItems] = React.useState([]);
+	return <div>DataProvider</div>;
+};
+
+export default DataProvider;
