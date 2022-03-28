6238599645788dfd592d711d8c30563324e89dab
alvaro-j <alvarojaime360@gmail.com>
2022-03-28 08:53:03 -0300

    create input state

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import FormInput from "./components/FormInput";
 
 const App: React.FC = () => {
 	// FC stands for functional component
+	const [input, setInput] = React.useState<string>("");
 	return (
 		<div className="main">
 			<h1>
 				buy<span>List</span>
 			</h1>
-			<FormInput />
+			<FormInput input={input} setInput={setInput} />
 		</div>
 	);
 };
