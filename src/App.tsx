9a5002d15de4a5d4e804f4dd5d4bd5705c9ecd54
alvaro-j <alvarojaime360@gmail.com>
2022-03-29 19:27:39 -0300

    import model and create the ItemArray state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import FormInput from "./components/FormInput";
+import { Item } from "./model";
 
 const App: React.FC = () => {
 	// FC stands for functional component
 	const [input, setInput] = React.useState<string>("");
+	const [itemArray, setItemArray] = React.useState<Item[]>([])
 	return (
 		<div className="main">
 			<h1>
